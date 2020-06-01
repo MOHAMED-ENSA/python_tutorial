f3a31757c62762b2ff91ab12d4f0eca96750b2e4
MOHAMED-ENSA2 <65856387+MOHAMED-ENSA2@users.noreply.github.com>
2020-06-01 16:04:51 +0100

    01-06-2020

--- python_mysql.py
+++ python_mysql.py
@@ -1,5 +1,7 @@
 # this is a tutorial about how to connect python to mysql server
 # get started 
+# establish commincation 
+
 # import mysql.connector
 
 # mydb = mysql.connector.connect(
@@ -84,14 +86,80 @@
 # mycursor.execute("CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
 
 # if the table already exist use the following code :  
+# import mysql.connector
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   passwd="12345",
+#   database="mydatabase"
+# )
+
+# mycursor = mydb.cursor()
+
+# mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+ 
+# fill a table  : 
+# import mysql.connector as mc 
+
+# mydb = mc.connect(
+#   host = "localhost",
+#   user = "root",
+#   passwd ="12345",
+#   database = "db2"
+# )
+# mycursor = mydb.cursor()
+
+# sql = "INSERT INTO students (name,CNE,class) VALUES (%s,%s,%s)"
+# exit = ""
+# col = 0 
+# while(exit != "exit") :
+#     name = input("enter your name :  ")
+#     CNE = int(input("enter your CNE :  "))
+#     Class = input("enter your class :  ")
+#     values  = (name,CNE,Class)
+#     mycursor.execute(sql,values)
+#     mydb.commit()
+#     exit = input("write exit to exit")
+#     col += 1 
+
+# #print(mycursor.rowcount , "record inserted")
+# print(f"{col} columns added ")
+
+# get the id for the the row added : 
+# import mysql.connector as mc
+# mydb = mc.connect(
+#   host = "localhost",
+#   user = "root",
+#   passwd ="12345",
+#   database = "db2"
+# )
+# mycursor = mydb.cursor()
+# sql = "INSERT INTO students (name,CNE,class) VALUES (%s,%s,%s)"
+# values  = ("soufian","1324567894","GI4")
+# mycursor.execute(sql,values)
+# mydb.commit()
+# print(mycursor.rowcount , "record inserted")
+# #print(f"{col} columns added ")
+
+# print("id value for column added is : ", mycursor.lastrowid)
+
+# select from a table : 
+
 import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   passwd="12345",
-  database="mydatabase"
+  database="db2"
 )
 
 mycursor = mydb.cursor()
+mycursor.execute("SELECt * FROM students")
+for x in mycursor : 
+  print(x)
 
-mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+
+
+
+
+
