48ffe919eb6d76834d6c993ce9af88c53588cc93
MOHAMED-ENSA2 <65856387+MOHAMED-ENSA2@users.noreply.github.com>
2020-07-14 23:05:30 +0100

    flask2

--- FlaskWeb.py
+++ FlaskWeb.py
@@ -1,12 +1,19 @@
 from flask import Flask , render_template
 fisrt_app = Flask(__name__)
 
+
+data = [("html" , 80) , ("css" , 60), ("js" , 75)]
 @fisrt_app.route("/")
 def homepage():
-    return render_template("main.html", pagetitle= "homepage" , test = "I'm the home page ")
+    return render_template("main.html", pagetitle= "homepage" , test = "page 1 :  " , mydata = data)
 
 @fisrt_app.route("/page2")
 def name(): 
-    return render_template("page2.html" , pagetitle ="about" , test = "I'm the about page ")
+    return render_template("page2.html" , pagetitle ="about" , test = "page 2 :  " , cssfile = "page2" )
+
+@fisrt_app.route("/add") 
+def add():
+    return render_template("add.html",pagetitle = "add" , test= 'page 3 : ' , cssfile = "add"  )
+
 if __name__ == "__main__" : 
     fisrt_app.run(debug=True , port=9000)
